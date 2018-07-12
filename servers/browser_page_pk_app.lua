194fc639534ccf95762a8c43bf0462a53bedc1cb
Gerold55 <marleybeare56@gmail.com>
2018-07-11 20:29:28 -0400

    Add files via upload

--- servers/browser_page_pk_app.lua
+++ servers/browser_page_pk_app.lua
@@ -24,7 +24,7 @@ function laptop.browser_api.header_receive_fields_func(app, mtos, sender, fields
 	end
 end
 
-laptop.register_view("servers.minetest/persistentkingdoms", {
+laptop.register_view("persistentkingdoms.servers.minetest", {
 	app_info = "Persistent Kingdoms Server Info",
 	browser_page = true,
 	browser_main_page = true,
@@ -37,7 +37,7 @@ laptop.register_view("servers.minetest/persistentkingdoms", {
 			mtos.theme:get_label('.1,2.7', 'Rules: See /rules ingame.')..
 			mtos.theme:get_label('.1,3.1', 'Mods: See topic for server in your web browser. https://forum.minetest.net/viewtopic.php?f=10&t=14587')..
 			mtos.theme:get_label('.1,3.5', 'Map:')..
-			mtos.theme:get_button('.6,3.55;5,.5', 'url_bright', 'page_link', 'servers.minetest/persistentkingdoms/map')..
+			mtos.theme:get_button('.6,3.55;5,.5', 'url_bright', 'page_link', 'persistentkingdoms.servers.minetest/map')..
 			mtos.theme:get_label('.1,3.9', 'Other Notes: Flags protect 64 blocks radius, in a sphere. Strict protocol checking is on, you will need Minetest 0.4.14 or newer to play.')..
 			"image[1,5;10,5;laptop_pk.png]"
 		return formspec
@@ -47,7 +47,7 @@ laptop.register_view("servers.minetest/persistentkingdoms", {
 	end
 })
 
-laptop.register_view("servers.minetest/persistentkingdoms/map", {
+laptop.register_view("persistentkingdoms.servers.minetest/map", {
 	app_info = "Persistent Kingdoms Server Map",
 	browser_page = true,
 	formspec_func = function(app, mtos)
--- servers/browser_page_servers_app.lua
+++ servers/browser_page_servers_app.lua
@@ -31,7 +31,7 @@ laptop.register_view("servers.minetest", {
 	formspec_func = function(app, mtos)
 		local formspec = laptop.browser_api.header_formspec_func(app, mtos) ..
 			mtos.theme:get_label('.1,1.2', 'Persistent Kingdoms') ..
-			mtos.theme:get_button('.1,1.7;4,.5', 'url_bright', 'page_link', 'servers.minetest/persistentkingdoms')
+			mtos.theme:get_button('.1,1.7;4,.5', 'url_bright', 'page_link', 'persistentkingdoms.servers.minetest')
 		return formspec
 	end,
 	receive_fields_func = function(app, mtos, sender, fields)
